ae558fef902139144beb51cfd49704add08f3ef6
Ivan Zaburdaev <ivanlom227@gmail.com>
2024-06-03 19:31:58 +0300

    fixed forwho padding

--- src/widgets/for-who/index.tsx
+++ src/widgets/for-who/index.tsx
@@ -16,36 +16,36 @@ const ForWho: FC = () => {
             src={'/images/forwho_footage1.png'}
             className={'w-full h-full absolute left-0 top-0 z-0'}
           />
-          <img className={'h-full'} src={'/images/forwho1.png'} />
+          <img className={'xl:w-auto w-2/3 xl:h-full'} src={'/images/forwho1.png'} />
           <div
             className={
               'for-who-text-grad w-full rounded-2xl -mt-10 p-8 flex flex-col gap-3'
             }>
-            <p className={'uppercase text-center text-4xl font-phil'}>
+            <p className={'uppercase text-center text-3xl xl:text-4xl font-phil'}>
               для пар
             </p>
-            <p className={'text-center text-[#393939] text-xl font-montserrat'}>
+            <p className={'text-center text-[#393939] text-base xl:text-xl font-montserrat'}>
               стремящимся разнообразить <br/> свои отношения
             </p>
           </div>
         </div>
         <div
           className={
-            'w-full xl:w-1/3 2xl:w-[36rem] rounded-2xl flex relative flex-col aspect-square for-who-footage'
+            'w-full xl:w-1/3 2xl:w-[36rem] rounded-2xl flex relative justify-center items-center flex-col aspect-square for-who-footage'
           }>
           <img
             src={'/images/forwho_footage2.png'}
             className={'w-full h-full absolute left-0 top-0 z-0'}
           />
-          <img src={'/images/forwho2.png'} />
+          <img className={'xl:w-auto w-2/3 xl:h-full'} src={'/images/forwho2.png'} />
           <div
             className={
               'for-who-text-grad w-full rounded-2xl -mt-10 p-8 flex flex-col gap-3'
             }>
-            <p className={'uppercase text-center text-4xl font-phil'}>
+            <p className={'uppercase text-center text-3xl xl:text-4xl font-phil'}>
               для одиночек
             </p>
-            <p className={'text-center text-[#393939] text-xl font-montserrat'}>
+            <p className={'text-center text-[#393939] text-base xl:text-xl font-montserrat'}>
               желающих лучше <br/>понять свое тело
             </p>
           </div>
